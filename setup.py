fd9360661ddc7aebdd7bf400574765030ec92ae4
w00fmeow <w00fmeow@protonmail.ch>
2022-08-18 13:19:34 +0300

    - bumped version

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     name='py_gitlab',
     author='Yoni Samoded @w00fmeow',
+    version='0.0.1',
     package_dir={"": "py_gitlab"},
     install_requires=[
         'asyncio == 3.4.3',
